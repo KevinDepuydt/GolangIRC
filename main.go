bbd3e5bf207519f5d564ad069b1865a4cc977b7d
KevinDepuydt <kevin.depuydt@supinternet.fr>
2016-12-13 18:13:49 +0100

    Add ng-file-upload module, golang upload handler

--- main.go
+++ main.go
@@ -5,7 +5,63 @@ import (
 	"net/http"
 
 	"./chat"
+	"fmt"
+	"os"
+	"io"
+	"strconv"
 )
+
+const uploadDir = "web/uploads/"
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	defer file.Close()
+
+	// get file name
+	filename := header.Filename
+
+	// check if file already exist
+	if _, err := os.Stat(uploadDir + filename); err == nil {
+		fileExist := true
+		i := 0
+		for fileExist {
+			// then change filename
+			log.Println("change filename try " + strconv.Itoa(i + 1))
+			temp := strconv.Itoa(i) + "-" + filename
+			if _, err := os.Stat(uploadDir + temp); os.IsNotExist(err) {
+				fileExist = false
+				filename = temp
+			}
+			i += 1
+		}
+	}
+
+	uploadPath := uploadDir + filename
+	out, err := os.Create(uploadPath)
+	if err != nil {
+		log.Println("Unable to create the file for writing. Check your write access privilege")
+		fmt.Fprintf(w, "Error, unable to save file")
+		return
+	}
+
+	defer out.Close()
+
+	// write the content from POST to the file
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
+	log.Println("File uploaded successfully!")
+	fmt.Fprintf(w, filename)
+}
 
 func main() {
 	log.SetFlags(log.Lshortfile)
@@ -16,6 +72,9 @@ func main() {
 
 	// static files
 	http.Handle("/", http.FileServer(http.Dir("web")))
+
+	// uploads
+	http.HandleFunc("/upload", uploadHandler);
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- web/js/app.js
+++ web/js/app.js
@@ -1,70 +1,101 @@
-angular.module('goChat', ['ngWebSocket', 'angularMoment'])
-  .controller('goChatController', ['$websocket', '$location', '$anchorScroll', 'amMoment', function($websocket, $location, $anchorScroll, amMoment) {
+// Constant
+const MSG_TYPE_MSG = 'chat_message';
+const MSG_TYPE_NOTIFICATION = 'notification';
+const NOTIFICATION_CLIENT_NUMBER = 'client_number';
 
-    amMoment.changeLocale('fr');
+// App
+angular.module('goChat', ['ngWebSocket', 'angularMoment', 'ngFileUpload'])
+  .controller('goChatController', ['$websocket', '$location', '$anchorScroll', 'amMoment', 'Upload',
+    function($websocket, $location, $anchorScroll, amMoment, Upload) {
+      // modules config
+      amMoment.changeLocale('fr');
 
-    var gc = this;
-    var ws = $websocket('ws://localhost:8080/chat');
+      // Controller variables
+      var gc = this;
+      var ws = $websocket('ws://localhost:8080/chat');
 
-    gc.nicknameIsChoosen = false;
-    gc.nickname = "";
-    gc.messages = [];
-    gc.clientNumber = 0;
-
-    gc.form = {
-      author: "Anonymous",
-      body: ""
-    };
-
-    gc.sendMessage = function() {
-      if (gc.form.author.length > 0 && gc.form.author !== "Anonymous") {
-        gc.nickname = gc.form.author;
-        gc.nicknameIsChoosen = true;
-      }
-      var message = Object.assign({ messageType: "chat_message", date: Date.now()}, gc.form);
-      console.log(message);
-      ws.send(message);
-      cleanForm();
-    };
-
-    const MSG_TYPE_MSG = 'chat_message';
-    const MSG_TYPE_NOTIFICATION = 'notification';
-    const NOTIFICATION_CLIENT_NUMBER = 'client_number';
-
-    ws.onMessage(function(message) {
-      var newMessage = JSON.parse(message.data);
-
-      console.log("New message ", newMessage);
-
-      switch (newMessage.messageType) {
-        case MSG_TYPE_MSG:
-          gc.messages.push(newMessage);
-          gc.scrollBottom();
-          break;
-        case MSG_TYPE_NOTIFICATION:
-          switch (newMessage.notificationType) {
-            case NOTIFICATION_CLIENT_NUMBER:
-              console.log("Client number has changed");
-              gc.clientNumber = newMessage.body;
-              break;
-            default:
-              console.log("Bad notification type");
-          }
-          break;
-        default:
-          console.log("Bad message type");
-      }
-    });
-
-    function cleanForm() {
+      gc.nicknameIsChoosen = false;
+      gc.nickname = "";
+      gc.messages = [];
+      gc.clientNumber = 0;
       gc.form = {
-        author: gc.nicknameIsChoosen ? gc.nickname : "Anonymous",
+        author: "Anonymous",
         body: ""
       };
-    }
 
-    gc.scrollBottom = function() {
-      $location.hash('sb');
-      $anchorScroll();
-    };
+      // Messages
+      gc.sendMessage = function() {
+        if (gc.form.author.length > 0 && gc.form.author !== "Anonymous") {
+          gc.nickname = gc.form.author;
+          gc.nicknameIsChoosen = true;
+        }
+        var message = Object.assign({ messageType: "chat_message", date: Date.now()}, gc.form);
+        console.log(message);
+        ws.send(message);
+        cleanForm();
+      };
+
+      ws.onMessage(function(message) {
+        var newMessage = JSON.parse(message.data);
+
+        console.log("New message ", newMessage);
+
+        switch (newMessage.messageType) {
+          case MSG_TYPE_MSG:
+            gc.messages.push(newMessage);
+            gc.scrollBottom();
+            break;
+          case MSG_TYPE_NOTIFICATION:
+            switch (newMessage.notificationType) {
+              case NOTIFICATION_CLIENT_NUMBER:
+                console.log("Client number has changed");
+                gc.clientNumber = newMessage.body;
+                break;
+              default:
+                console.log("Bad notification type");
+            }
+            break;
+          default:
+            console.log("Bad message type");
+        }
+      });
+
+      // Uploads
+      gc.uploadMessage = "";
+      gc.uploadFileSrc = "";
+      gc.uploadSuccess = false;
+
+      gc.upload = function (file) {
+        gc.uploadMessage = "";
+        gc.uploadFileSrc = "";
+        gc.uploadSuccess = false;
+        Upload.upload({
+          url: '/upload',
+          data: {file: file}
+        }).then(function (res) {
+          console.log('Success : file name return by server ' + res.data);
+          gc.uploadMessage = "Le fichier à bien été téléchargé";
+          gc.uploadFileSrc = res.data;
+          gc.uploadSuccess = true;
+        }, function (res) {
+          console.log('Error status: ' + res.status);
+        }, function (evt) {
+          var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+          console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+      };
+
+      // Clean message form
+      function cleanForm() {
+        gc.form = {
+          author: gc.nicknameIsChoosen ? gc.nickname : "Anonymous",
+          body: ""
+        };
+      }
+
+      // Auto scroll bottom on new message
+      gc.scrollBottom = function() {
+        $location.hash('sb');
+        $anchorScroll();
+      };
   }]);
